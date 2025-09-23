379b140d01b2a0ead43dd980a8eb4330889156e1
Marcello Bozzi <bozzimarcello@gmail.com>
2025-09-23 10:54:41 +0200

    neural network arrays definitions

--- main.c
+++ main.c
@@ -11,5 +11,14 @@ int main (void) {
   
   const double lr = 0.1f;
 
+  double hiddenLayer[NUM_HIDDEN_NODES];
+  double outputLayer[NUM_OUTPUTS];
+
+  double hiddenLayerBias[NUM_HIDDEN_NODES];
+  double outputLayerBias[NUM_OUTPUTS];
+
+  double hiddenWeights[NUM_INPUTS][NUM_HIDDEN_NODES];
+  double outputWeights[NUM_HIDDEN_NODES][NUM_OUTPUTS];
+
   
 }
